c5d3216ee9fb79accb7ce752315a6d091d901b74
dpsong <57675424+dpsong@users.noreply.github.com>
2021-04-02 11:20:00 +0800

    Update vfoct_attention.py

--- src/models/vfoct_attention.py
+++ src/models/vfoct_attention.py
@@ -11,17 +11,12 @@ class VfOctAttentionNet(nn.Module):
         super(VfOctAttentionNet, self).__init__()
         self.octnet = OctExtractor()
         self.vfnet = VfExtractor()
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.alpha = AttentionNet()
         self.fcnet = FCNet(in_channels=80)
 
     def forward(self, oct_in, vf_in):
         oct_fea = self.octnet(oct_in)
-        oct_fea = self.avgpool(oct_fea)
-
         vf_fea = self.vfnet(vf_in)
-        vf_fea = self.avgpool(vf_fea)
-
         fusion_fea = self.alpha(oct_fea, vf_fea)
         out = self.fcnet(fusion_fea)
         
